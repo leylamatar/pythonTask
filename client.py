402d6563c1487bd9f2a98223479aea07fccf0da7
Leyla Matar <matarlaila10@gmail.com>
2024-03-09 04:10:04 +0300

    post request

--- client.py
+++ client.py
@@ -1,1 +1,62 @@
+import io
+import requests
+import csv
+import pandas as pd
+from datetime import datetime, timedelta
 
+def send_csv_to_server(csv_file, keys=None, colored=True):
+    with open(csv_file, 'rb') as file:
+        csv_content = file.read().decode("utf-8")
+
+    params = {"colored": colored}
+    if keys:
+        params["keys"] = keys
+
+    response = requests.post("http://127.0.0.1:8000/vehicle_info/", params=params, files={"csv_file": io.StringIO(csv_content)})
+
+    if response.status_code == 200:
+        #save the CSV content
+        csv_content = response.text.splitlines()
+        df = pd.read_csv(io.StringIO("\n".join(csv_content)), header=None)  
+
+        # Rows are sorted by response field 
+        df = df.sort_values(by=0) 
+        # Columns always contain rnr field
+        if 'rnr' not in df.columns:
+            df.insert(0, 'rnr', None)
+        # Save as Excel file
+        current_date_iso_formatted = datetime.now().strftime("%Y-%m-%d")
+        excel_filename = f'vehicles_{current_date_iso_formatted}.xlsx'
+        df.to_excel(excel_filename, index=False)
+
+        return excel_filename
+    else:
+        print("Error:", response.text)
+        return None
+    
+
+
+def color_dataframe(df):
+    # 'hu' color
+    today = datetime.now()
+    three_months_ago = today - timedelta(days=3*30)
+    twelve_months_ago = today - timedelta(days=12*30)
+
+    df['color'] = ''
+    df.loc[df['hu'] >= three_months_ago, 'color'] = '#007500'  # green
+    df.loc[(df['hu'] >= twelve_months_ago) & (df['hu'] < three_months_ago), 'color'] = '#FFA500'  # orange
+    df.loc[df['hu'] < twelve_months_ago, 'color'] = '#b30000'  # red
+
+    return df
+
+
+if __name__ == "__main__":
+    #change the the file name and keys (optional)
+    csv_file_path = "vehicles.csv"
+    keys = ["kurzname", "info"]
+    colored = True
+    excel_filename = send_csv_to_server(csv_file_path, keys=keys, colored=colored)
+    if excel_filename:
+        print(f"Excel file created: {excel_filename}")
+    else:
+        print("Failed to generate Excel file.")
--- server.py
+++ server.py
@@ -1,8 +1,9 @@
-from fastapi import FastAPI, HTTPException, Query
-import requests
-from datetime import datetime
 import csv
+import json
+from fastapi import FastAPI, Response, UploadFile, File, HTTPException, Query
+import requests
 import pandas as pd
+import uvicorn
 
 app = FastAPI()
 
@@ -26,12 +27,28 @@ def resolve_color_code(label_id):
     label_info = label_response.json()
     return label_info.get("colorCode", "blue")  # return colorCode as default
 
+
+@app.post("/vehicle_info/")
+async def post_vehicle_info(csv_file: UploadFile = File(...)):
+   # read file content
+    csv_content = await csv_file.read()
+    csv_reader = csv.DictReader(csv_content.decode("utf-8").splitlines())
+
+    # Filter out any resources that do not have a value set for hu field
+    vehicle_info = [row for row in csv_reader if row.get("hu") is None]
+
+    return {
+        "original_data": vehicle_info,
+    }
+
+
+
 @app.get("/vehicle_info/")
 async def get_vehicle_info_data(colored: bool = Query(True, description="Flag to resolve color codes for labelIds")):
     url = "https://api.baubuddy.de/index.php/login"
     payload = {
-        "username": "",
-        "password": ""
+        "username": "365",
+        "password": "1"
     }
     headers = {
         "Authorization": "Basic QVBJX0V4cGxvcmVyOjEyMzQ1NmlzQUxhbWVQYXNz",
@@ -43,7 +60,6 @@ async def get_vehicle_info_data(colored: bool = Query(True, description="Flag to
         raise HTTPException(status_code=response.status_code, detail="Failed to authenticate")
     
     access_token = response.json().get("oauth", {}).get("access_token")
-    
     vehicle_info = get_vehicle_info(access_token)
     
     if colored:
@@ -56,28 +72,10 @@ async def get_vehicle_info_data(colored: bool = Query(True, description="Flag to
                     color_code = resolve_color_code(label_id)
                     vehicle["labelColors"].append(color_code)
     
-    # Filter out any resources that do not have a value set for hu field
-    filtered_vehicle_info = [vehicle for vehicle in vehicle_info if vehicle.get("hu") is None]
-    
-    # return data-structure in JSON format
+    # return data in JSON format
     return {
-        "original_data": vehicle_info,
-        "filtered_data": filtered_vehicle_info
+        "original_data2": vehicle_info,
     }
 
 if __name__ == "__main__":
-    import uvicorn
     uvicorn.run(app, host="127.0.0.1", port=8000)
-
-######TO GET TOKEN 
-# url = "https://api.baubuddy.de/index.php/login"
-# payload = {
-#     "username": "",
-#     "password": ""
-# }
-# headers = {
-#     "Authorization": "Basic QVBJX0V4cGxvcmVyOjEyMzQ1NmlzQUxhbWVQYXNz",
-#     "Content-Type": "application/json"
-# }
-# response = requests.request("POST", url, json=payload, headers=headers)
-# print(response.text)
